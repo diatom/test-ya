cda68108edde495201ce73048b5f97b016cb5e3b
Severin <Supriore@gmail.com>
2024-09-09 10:47:20 +0300

    update

--- site.mjs
+++ site.mjs
@@ -156,6 +156,7 @@ function Layout(tit, desc, img, ...chi) {
         E.meta.props({property: `og:image:type`, content: `image/jpeg`}),
         E.link.props({rel: `icon`, type: `image/x-icon`, href: `/images/icon.ico`}),
         E.link.props({rel: `stylesheet`, href: `/main.css`}),
+        E.style.chi(`@import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap')`),
         E.style.chi(`@import url('https://fonts.googleapis.com/css2?family=Geologica:wght,CRSV,SHRP@100..900,0..1,0..100&family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap');`),
         a.vac(DEV) && E.script.chi(`navigator.serviceWorker.register('/sw.mjs')`),
         Md(`./data/anal.md`)
