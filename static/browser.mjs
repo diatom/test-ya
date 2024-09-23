7a841a277f7fc962848087ef8b9c0d7199718329
Severin <Supriore@gmail.com>
2024-09-23 09:35:43 +0300

    update

--- static/browser.mjs
+++ static/browser.mjs
@@ -9,169 +9,136 @@ document.addEventListener("DOMContentLoaded", function() {
     const sliderNum = document.querySelector('.slider-num')
     const totalSlides = slides.length
     let slideInterval
-
     let visibleSlides
-    
     function updateVisibleSlides() {
         const containerWidth = document.querySelector('.slides-container').offsetWidth
         if (containerWidth >= 1140) {
-            visibleSlides = 3 // Если ширина контейнера 1222px или больше
+            visibleSlides = 3
         } else if (containerWidth >= 788) {
-            visibleSlides = 2 // Если ширина контейнера от 830px до 1219px
+            visibleSlides = 2
         } else if (containerWidth >= 394) {
-            visibleSlides = 1 // Если ширина контейнера от 420px до 829px
+            visibleSlides = 1
         } else {
             visibleSlides = 1
         }
     }
-
-    // Инициализация при загрузке страницы
     updateVisibleSlides()
-    
-    // Обновление при изменении размера окна
+     
     window.addEventListener('resize', updateVisibleSlides)
-    
-    let currentSlideGroup = 0 // Индекс группы слайдов
-
+    let currentSlideGroup = 0
     function updateSliderNum() {
         sliderNum.textContent = (currentSlideGroup + 1) + ' / ' + Math.ceil(totalSlides / visibleSlides)
     }
-
     function goToSlide(groupIndex) {
         slidesWrap.style.transform = 'translateX(' + (-groupIndex * (100 / Math.ceil(totalSlides / visibleSlides))) + '%)'
         currentSlideGroup = groupIndex
         updateSliderNum()
     }
-
     function nextSlide() {
-        const totalGroups = Math.ceil(totalSlides / visibleSlides) // Общее количество групп слайдов
-        currentSlideGroup = (currentSlideGroup + 1) % totalGroups // Переход к следующей группе
+        const totalGroups = Math.ceil(totalSlides / visibleSlides)
+        currentSlideGroup = (currentSlideGroup + 1) % totalGroups
         goToSlide(currentSlideGroup)
     }
-
     function prevSlide() {
-        const totalGroups = Math.ceil(totalSlides / visibleSlides) // Общее количество групп слайдов
-        currentSlideGroup = (currentSlideGroup - 1 + totalGroups) % totalGroups // Переход к предыдущей группе
+        const totalGroups = Math.ceil(totalSlides / visibleSlides)
+        currentSlideGroup = (currentSlideGroup - 1 + totalGroups) % totalGroups
         goToSlide(currentSlideGroup)
     }
-
     nextButton.addEventListener('click', function() {
         nextSlide()
         resetInterval()
     })
-
     prevButton.addEventListener('click', function() {
         prevSlide()
         resetInterval()
     })
-
     function resetInterval() {
         clearInterval(slideInterval)
         slideInterval = setInterval(nextSlide, 4000)
     }
-
     slideInterval = setInterval(nextSlide, 4000)
-    
-    // Инициализация номера слайда
     updateSliderNum()
 })
-
 
 window.addEventListener('resize', () => {
     const steps = document.querySelectorAll('.step')
     const dots = document.querySelectorAll('.step-dot')
     const leftArrow = document.getElementById('step-arrow-left')
     const rightArrow = document.getElementById('step-arrow-right')
-    let currentStep = 0 // Start with step-1
-
+    let currentStep = 0
     if (window.matchMedia('(max-width: 859px)').matches) {
-        // Код для выполнения при изменении размера окна меньше 860px
         setStepStyles(currentStep)
         updateUI()
 
-        // Event listeners for navigation arrows
         rightArrow.addEventListener('click', function() {
-            if (currentStep < 4) { // Allow switching only up to the last case (dot-5)
+            if (currentStep < 4) {
                 currentStep++
                 setStepStyles(currentStep)
                 updateUI()
             }
         })
-
         leftArrow.addEventListener('click', function() {
-            if (currentStep > 0) { // Allow switching only down to the first case (dot-1)
+            if (currentStep > 0) {
                 currentStep--
                 setStepStyles(currentStep)
                 updateUI()
             }
         })
-
-        // Function to set styles based on active dot
         function setStepStyles(index) {
-            // Hide all steps initially
             steps.forEach(step => {
                 step.style.display = 'none'
             })
-
             switch (index) {
-                case 0: // dot-1 active
+                case 0:
                     steps[0].style.gridColumn = '1'
                     steps[0].style.gridRow = '1'
-                    steps[0].style.display = 'flex' // Show step-1
+                    steps[0].style.display = 'flex'
                     steps[1].style.gridColumn = '1'
                     steps[1].style.gridRow = '2'
-                    steps[1].style.display = 'flex' // Show step-2
+                    steps[1].style.display = 'flex'
                     break
-                case 1: // dot-2 active
+                case 1:
                     steps[2].style.gridColumn = '1'
                     steps[2].style.gridRow = '1 / 3'
-                    steps[2].style.display = 'flex' // Show step-3
+                    steps[2].style.display = 'flex'
                     break
-                case 2: // dot-3 active
+                case 2:
                     steps[3].style.gridColumn = '1'
                     steps[3].style.gridRow = '1'
-                    steps[3].style.display = 'flex' // Show step-4
+                    steps[3].style.display = 'flex'
                     steps[4].style.gridColumn = '1'
                     steps[4].style.gridRow = '2'
-                    steps[4].style.display = 'flex' // Show step-5
+                    steps[4].style.display = 'flex'
                     break
-                case 3: // dot-4 active
+                case 3:
                     steps[5].style.gridColumn = '1'
                     steps[5].style.gridRow = '1 / 3'
-                    steps[5].style.display = 'flex' // Show step-6
+                    steps[5].style.display = 'flex'
                     break
-                case 4: // dot-5 active
+                case 4:
                     steps[6].style.gridColumn = '1'
                     steps[6].style.gridRow = '1 / 3'
-                    steps[6].style.display = 'flex' // Show step-7
+                    steps[6].style.display = 'flex'
                     break
             }
         }
-
         function updateUI() {
-            // Update dots' colors
             dots.forEach((dot, index) => {
-                dot.style.backgroundColor = index === currentStep ? '#313131' : '' // Change color for active dot
+                dot.style.backgroundColor = index === currentStep ? '#313131' : ''
             })
-
-            // Update arrow colors based on the current step
-            leftArrow.style.backgroundColor = currentStep === 0 ? '' : '#313131' // Default color if at the first step
-            rightArrow.style.backgroundColor = currentStep === 4 ? '' : '#313131' // Default color if at the last step
+            leftArrow.style.backgroundColor = currentStep === 0 ? '' : '#313131'
+            rightArrow.style.backgroundColor = currentStep === 4 ? '' : '#313131'
         }
-
     } else if (window.matchMedia('(min-width: 860px)').matches) {
-        // Код для выполнения при изменении размера окна больше или равно 860px
-        resetSteps() // Сброс стилей для всех шагов
-
+        resetSteps()
         function resetSteps() {
             steps.forEach(step => {
-                step.style.display = 'flex'   // Показываем все шаги в flex режиме
-                step.style.gridColumn = ''     // Сбрасываем grid-column, если необходимо 
-                step.style.gridRow = ''        // Сбрасываем grid-row, если необходимо 
+                step.style.display = 'flex'
+                step.style.gridColumn = ''
+                step.style.gridRow = ''
             })
         }
     }
 })
-
-// Инициализация при загрузке страницы
 window.dispatchEvent(new Event('resize'))
+console.log(`O̲ppa̲ (っ-̶●̃益●̶̃)っ ,︵‿ S̲t̲yl̲e̲`)
